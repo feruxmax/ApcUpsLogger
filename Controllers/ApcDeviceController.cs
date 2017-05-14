8b11728b3059939a155386be38c09a00e55002da
feruxmax <feruxmax@gmail.com>
2017-05-14 18:35:59 +0300

    Add WebApi project

---
+++ Controllers/ApcDeviceController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ApcUpsLogger.Engine;
+
+namespace ApcUpsLogger.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApcDeviceController : Controller
+    {
+        private readonly ApcDevice apcDevice;
+
+        public ApcDeviceController()
+        {
+            apcDevice = new ApcDevice();
+        }
+
+        // GET api/status
+        [HttpGet("status")]
+        public string GetStatus()
+        {
+            return String.Join("", apcDevice.GetStatus());
+        }
+
+        // GET api/linev
+        [HttpGet("linev")]
+        public string GetLineV()
+        {
+            return apcDevice.GetParamValue("LINEV");
+        }
+    }
+}
