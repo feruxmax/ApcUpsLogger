fd812352a72b9f2483c4eead37610801478e4361
feruxmax <feruxmax@gmail.com>
2017-05-15 19:25:03 +0300

    add db script

---
+++ scripts/initial.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE public.tlinevoltages (
+  id 	     serial NOT NULL,
+  value      double precision NOT NULL,
+  "date"     timestamp WITHOUT TIME ZONE NOT NULL DEFAULT timezone('utc'::text, now()),
+  	
+  CONSTRAINT tlinevoltages_pkey
+    PRIMARY KEY (id)
+) WITH (
+    OIDS = FALSE
+);
+ALTER TABLE public.tlinevoltages
+  OWNER TO application;
